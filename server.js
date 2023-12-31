edfdaeed293e57f31bc3ae17020d823b4e1b268b
huxiaolux <huxiaolux@outlook.com>
2023-12-31 14:34:42 +0800

    代理解决跨域

--- server.js
+++ server.js
@@ -1,57 +1,39 @@
 const express = require('express');
 const {createProxyMiddleware} = require('http-proxy-middleware');
+const cors = require('cors');
 
-const app = express();
+// 创建代理服务器函数
+function createProxyServer({path, target}) {
+    const app = express();
+    app.use(cors());
 
-// 配置代理列表
-const proxyList = [
-    {
-        path: '/TEST_API',
-        target: 'https://feiying.ahjinti.cn',//师傅驿站
-        port: 8086,
-    },
-];
-
-// 循环创建代理中间件并应用
-proxyList.forEach((proxy) => {
-    const {path, target, port} = proxy;
-
-    const apiProxy = createProxyMiddleware(path, {
+    app.use(path, createProxyMiddleware({
         target,
         changeOrigin: true,
         onProxyRes(proxyRes, req, res) {
-            // 允许跨域访问
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-            // 监听目标返回的数据
-            /*let responseData = '';
-            proxyRes.on('data', (chunk) => {
-                responseData += chunk;
-            });
-            proxyRes.on('end', () => {
-                try {
-                    const formattedData = JSON.parse(responseData);
-                    console.log('格式化后的数据:', formattedData);
-                } catch (error) {
-                    console.error('数据解析错误:', error);
-                    console.log('原始数据:', responseData);
-                }
-            });*/
         },
         pathRewrite: {
-            [`^${path}`]: '', // 将路径中的代理路径移除
+            [`^${path}`]: '',
         },
-        onError(err, req, res) {
-            console.error('代理错误:', err);
-            res.status(500).send('代理错误');
-        },
-    });
+    }));
 
-    app.use('/', apiProxy);
+    return app;
+}
 
-    // 启动服务器
-    app.listen(port, () => {
-        console.log(`服务器${port}已启动`);
+// 配置每个代理服务器
+const proxyConfigurations = [
+    {path: '/api', target: 'https://feiying.ahjinti.cn'},
+    // {path: '/anotherApi', target: 'https://another-server.example.com'},
+];
+
+// 启动多个代理服务器
+proxyConfigurations.forEach(({path, target}, index) => {
+    const app = createProxyServer({path, target});
+    const PORT = 3000 + index; // 3000, 3001, ...
+
+    app.listen(PORT, () => {
+        console.log(`代理服务器 ${index + 1} http://localhost:${PORT} 已启动，监听端口 ${PORT}`);
     });
 });
